f3c56f8e38f8334ad47e7053f1d4ed73b66a2343
Dion Kanhai <dionkanhai@Dions-MacBook-Air.local>
2022-12-03 17:06:42 -0500

    add daylist to application and verify it logs the day in console

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,7 +6,7 @@ export default function DayList(props) {
   const listOfDaysAndSlots = props.days.map(item => {
     return (
       <DayListItem
-        id={item.id}
+        key={item.id}
         name={item.name}
         spots={item.spots}
         selected={item.name === props.day}
