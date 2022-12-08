308e77d68a69259c6deee2d4a83d1a6129e1182c
Dion Kanhai <dionkanhai@Dions-MacBook-Air.local>
2022-12-07 20:51:47 -0500

    display appointments

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import "index.scss";
@@ -15,6 +15,8 @@ import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
 import Form from "components/Appointment/Form";
+
+
 
 
 
@@ -209,4 +211,22 @@ storiesOf("Appointment", module)
     onSave={action("onSave")}
     onCancel={action("onCancel")}
   />
+))
+.add("Appointment Empty", () => (
+  <Fragment>
+    <Appointment id={1} time="4pm" />
+    <Appointment time="5pm" />
+  </Fragment>
+))
+.add("Appointment Booked", () => (
+  <Fragment>
+    <Appointment 
+      id={1}
+      time="4pm"
+      interview={{student: "Lydia Miller-Jones", interviewer}}
+    />
+    <Appointment
+      time="5pm"
+    />
+  </Fragment>
 ))
