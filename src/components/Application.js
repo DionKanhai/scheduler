b053c3c2c8a92338e8900b036a623b4e94839a5a
Dion Kanhai <dionkanhai@Dions-MacBook-Air.local>
2022-12-17 17:57:24 -0500

    implement the getInterview function to fetch interviews from the api

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -11,32 +11,37 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: 'Monday',
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
 
   const setDay = day => setState({ ...state, day });
 
+
   useEffect(() => {
     const daysURL = `http://localhost:8001/api/days`;
     const appointmentsURL = `http://localhost:8001/api/appointments`;
+    const interviewersURL = `http://localhost:8001/api/interviewers`;
     Promise.all([
       axios.get(daysURL),
-      axios.get(appointmentsURL)
+      axios.get(appointmentsURL),
+      axios.get(interviewersURL)
     ]).then((all) => {
-      setState(prev => (({...prev, days: all[0].data, appointments: all[1].data})))
+      setState(prev => (({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data})))
     })
   }, []);
 
-  // const dailyAppointments = [];
-  // console.log(state.day)
-  const dailyAppointments = getAppointmentsForDay(state, state.day)
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day)
   const appointmentsData = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview)
     return (
       <Appointment
         key={appointment.id}
-        {...appointment}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
     )
   })
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -11,3 +11,21 @@ export function getAppointmentsForDay(state, day) {
   }
   return results;
 }
+
+/* this function takes a state and a interview as an 
+input and returns an object that contains the interview data
+if it is passed an object that contains an interviewer */
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewerID = interview.interviewer
+  const interviewObj = {
+    student: interview.student,
+    interviewer: state.interviewers[interviewerID]
+  };
+  return interviewObj;
+};
+
+
+
